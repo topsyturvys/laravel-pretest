388636100fa6d2a12a533f431b2b6c905ec398ec
topsyturvys <topsyturvy.crazed@gmail.com>
2025-08-03 15:50:28 +0900

    ver02

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <div class="header-utilitites">
+                <a href="#" class="header__logo">
+                    検索画面
+                </a>
+                <form action="" method="post">
+                    <button class="header__button">
+                        登録画面へ
+                    </button>
+                </form>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="search">
+            <form action="" class="search-form">
+                <div class="search-form__item">
+                    <input class="search-from__item-input" type="text">
+                </div>
+                <div class="search-form__button">
+                    <button class="search-form__button-submit" type = "submit">
+                        検索
+                    </button>
+                </div>
+
+            </form>
+        </div>
+    </main>
+</body>
+
+</html>
